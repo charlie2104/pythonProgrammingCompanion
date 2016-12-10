9cacb69439e3229f6612adb5d5a823080f823a29
charlie bishop <charliebishop21@live.co.uk>
2016-12-10 16:30:53 +0000

    completed the test marks challenge

---
+++ objective2/testMarks.py
@@ -0,0 +1,8 @@
+#finds the average of three test marks
+
+mark1 = float(input("test mark 1: "))
+mark2 = float(input("test mark 2: "))
+mark3 = float(input("test mark 3: "))
+
+average = (mark1+mark2+mark3)/3
+print("the average is " + str(average))
