0d446aeca7976d3da5b2dc65384ab1e694d4e001
charlie bishop <charliebishop21@live.co.uk>
2016-12-10 16:17:59 +0000

    completed the visual dice challenge

---
+++ objective1/VisualDiceChallenge.py
@@ -0,0 +1,9 @@
+//outputs the number 5 on a dice
+
+print("oooooooooooo")
+print("o          o")
+print("o  #    #  o")
+print("o    #     o")
+print("o  #    #  o")
+print("o          o")
+print("oooooooooooo")
